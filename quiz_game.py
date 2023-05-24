38dc050a360a48ef9d5b8618117e99787cb3141f
Apoorva-Singh3 <27.apoorva@gmail.com>
2023-05-24 13:29:26 +0530

    first commit

---
+++ quiz_game.py
@@ -0,0 +1,49 @@
+quiz = {
+    "Question 1" : {
+        "question" : "What is the Capital of France ?",
+        "answer" : "Paris"
+    },
+    "Question 2" : {
+        "question" : "What is the Capital of Germany ?",
+        "answer" : "Berlin"
+    },
+    "Question 3" : {
+        "question" : "What is the Capital of Spain ?",
+        "answer" : "Madrid"
+    },
+    "Question 4" : {
+        "question" : "What is the Capital of Portugal ?",
+        "answer" : "Lisbon"
+    },
+    "Question 5" : {
+        "question" : "What is the Capital of Switzerland ?",
+        "answer" : "Bern"
+    },
+    "Question 6" : {
+        "question" : "What is the Capital of Italy ?",
+        "answer" : "Rome"
+    },
+    "Question 7" : {
+        "question" : "What is the Capital of Austria ?",
+        "answer" : "Vienna"
+    }
+}
+
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input('Answer ?')
+    
+    if answer.lower() == value['answer'].lower():
+        print("Correct")
+        score += 1
+        print('Your score is : ' + str(score))
+        print('Your percentage is ' + str(int(score/7 * 100)) + '%')
+        print('')
+        print('')
+    else:
+        print('Wrong !')
+        print('Your score is ' + str(score))
+        print('')
+        print('')
